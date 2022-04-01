87874dad9c740d770e649340a2760d98e3343bf0
Sejal Rajput <sejaljrajput@gmail.com>
2022-04-01 15:16:20 +0530

    project uploaded

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -131,5 +132,6 @@ LOGIN_URL = "login"
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
